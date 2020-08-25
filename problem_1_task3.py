8cfb6e358e0ea05315b65b62ae2ddcee55fad9b9
Swami Ganesh <62614711+ganesh1729ganesh@users.noreply.github.com>
2020-08-26 00:47:31 +0530

    Add files via upload

---
+++ problem_1_task3.py
@@ -0,0 +1,38 @@
+
+n = input("Enter the number: ")
+#Taking input in the form of string
+
+  
+   
+sumTemp = 0
+count = 0
+  
+   
+while (len(n) > 1): #only we need to find sum of digits only when it is a non-single digit number
+      
+    sumTemp = 0
+  
+
+    for i in range(len(n)): 
+        sumTemp +=  int(n[i])# simply to convert the digit in the form of character to int form in each iteration
+                                                    
+  
+                              
+        # converting temporary_sum into  
+        # string str again . 
+    n = str(sumTemp)# for loop to continue AND FOR  len method to work 
+  
+        # increase the count  for each loop and that gives finally the least no. of times we need to add
+    count += 1
+
+print(count)
+
+
+""" TIME COMPLEXITY:
+                    2 loops 1 loop inside another loop , so O(n^2)
+
+    SPACE COMPLEXITY:
+                    SINCE  onnly one int input and few constant space taking integers
+
+                    overall space complexity is O(1) """
+                    
